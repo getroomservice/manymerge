9b6a368a4fc35931664e44d6beb5875f96bbb633
Speros Kokenes <speroskokenes@gmail.com>
2021-02-09 14:03:20 -0500

    benchmarking; hub messageApply buffered

--- src/hub.ts
+++ src/hub.ts
@@ -1,4 +1,4 @@
-import { applyChanges, Doc, getChanges } from 'automerge';
+import { applyChanges, Change, Doc, getChanges } from 'automerge';
 import { getClock, later, recentChanges, union } from 'automerge-clocks';
 import { Map, fromJS } from 'immutable';
 import { Clock, Message } from './types';
@@ -33,6 +33,90 @@ export class Hub {
     this._theirClocks = Map();
     this._broadcast = broadcastMsg;
     this._sendTo = sendMsgTo;
+  }
+
+  public applyMessageBuffered<T>(
+    msgs: { peerId: string; msg: Message }[],
+    doc: Doc<T>
+  ): Doc<T> | undefined {
+    // Keep a list of the peer clocks in this message buffer
+    const peerClocks = Map<string, Clock>();
+    // A merge of all changes in the buffer
+    let combinedChanges: Change[] = [];
+
+    // Pull out all the clocks and merge all the changes
+    for (var i = 0; i < msgs.length; i++) {
+      const call = msgs[i];
+      peerClocks.set(call.peerId, call.msg.clock);
+      this._theirClocks = this._theirClocks.set(call.peerId, call.msg.clock);
+      combinedChanges = combinedChanges.concat(call.msg.changes ?? []);
+    }
+
+    let ourDoc = doc;
+
+    // Keep track of whether we've broadcast our clock or not
+    let hasBroadcastClock = false;
+
+    // 1. If we received changes, try to apply them
+    if (combinedChanges.length > 0) {
+      // We apply changes locally and update our clock before broadcasting.
+      // This way, in case the broadcast causes new messages to be delivered to us
+      // synchronously, our clock is uptodate.
+      ourDoc = applyChanges(doc, combinedChanges);
+      // Determine the net new changes for the hub's doc based on the incoming message
+      const newChanges = getChanges(doc, ourDoc);
+      this._ourClock = getClock(ourDoc);
+      // We broadcast FIRST for the other members of the hub
+      // Since we'll assume these changes should be applied to everyone.
+      // We only broadcast any changes that are new to this hub
+      if (newChanges.length > 0) {
+        this.broadcastMsg({
+          clock: getClock(ourDoc),
+
+          // We make the assumption that if someone's sent the hub
+          // changes, they likely want those changes to be sent to
+          // everyone else.
+          changes: newChanges,
+        });
+        hasBroadcastClock = true;
+      }
+    }
+
+    // 2. If we have any changes to let them know about,
+    // we should send it to them.
+    // Also loop through to see if we have any peers with earlier clocks
+    let hasDetectedPeerWithEarlierClock = false;
+    peerClocks.forEach((peerClock, peerId) => {
+      const ourChanges = recentChanges(ourDoc, peerClock!);
+      if (ourChanges.length > 0) {
+        this.sendMsgTo(peerId!, {
+          clock: getClock(ourDoc),
+          changes: ourChanges,
+        });
+      } else if (
+        later(peerClock!, this._ourClock) &&
+        !hasDetectedPeerWithEarlierClock
+      ) {
+        hasDetectedPeerWithEarlierClock = true;
+      }
+    });
+
+    // 3. If their clock is later than our clock,
+    // then we should let them know, which will prompt
+    // them to send us changes via 2. listed above.
+    // TO DO: does this need to be broadcast? Or just sent to individual peer as part of step 2?
+    if (hasDetectedPeerWithEarlierClock && !hasBroadcastClock) {
+      this.broadcastMsg({
+        clock: this._ourClock,
+      });
+    }
+
+    // Finally, we we made changes, we should return the
+    // doc to be cached. Otherwise return nothing.
+    if (combinedChanges.length > 0) {
+      return ourDoc;
+    }
+    return;
   }
 
   public applyMessage<T>(
@@ -148,9 +232,11 @@ export class Hub {
 
     this._ourClock = msg.clock;
 
-    // Todo: maybe do this asynchronously to not block in big rooms?
-    this._theirClocks = this._theirClocks.map(clock => {
-      return union(clock!, msg.clock);
-    }) as Map<string, Clock>;
+    // Update their clocks on next loop
+    setTimeout(() => {
+      this._theirClocks = this._theirClocks.map(clock => {
+        return union(clock!, msg.clock);
+      }) as Map<string, Clock>;
+    }, 0);
   }
 }
